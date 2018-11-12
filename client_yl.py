23d5550688bfbeb79124d344c362af8dd14ee4fc
smartlearner1520 <32616838+smartlearner1520@users.noreply.github.com>
2018-11-13 04:02:57 +0800

    Update client_yl.py
    by yl 2018/11/13 4.02am

--- client_yl.py
+++ client_yl.py
@@ -5,7 +5,7 @@ import struct
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(0.6)
+sock.settimeout(5)
 
 
 # Connect the socket to the port where the server is listening
@@ -46,6 +46,7 @@ cache = [packet]
 temp_ack_list = []
 seq_number = next_acknowledgement_number
 packet_size = 24
+temp_seq_number = 0
 
 while True:
     print(cwnd_size)
@@ -59,40 +60,62 @@ while True:
             temp_ack_list.append(recv_ack_number)
             recv_buffer = header_content[2]
         max_cwnd_size = recv_buffer // packet_size
-        print(data[11:])
+        #print(data[11:])
         print (seq_number)
-        print(temp_ack_list)
+        print(max(temp_ack_list))
         if max(temp_ack_list) == seq_number:
             cache = cache[cwnd_size:]
+            print("Cache size ", len(cache))
             if congestion_state:
                 cwnd_size += 1
             else:
                 cwnd_size *= 2
-            # if cwnd_size > max_cwnd_size:
-                # cwnd_size = max_cwnd_size
+            #if cwnd_size > max_cwnd_size:
+                #cwnd_size = max_cwnd_size
         else:
+            print("------1-------", max(temp_ack_list))
+            print(temp_ack_list)
             cache = cache[temp_ack_list.index(max(temp_ack_list)):]
             if cwnd_size == 1:
                 cwnd_size = 1
             else:
                 cwnd_size = cwnd_size // 2
             congestion_state = 1
-        seq_number = max(temp_ack_list)
+            #print("-------4-------", seq_number)
+            if len(temp_ack_list) ==1:
+                temp_seq_number = max(temp_ack_list)-13
+            else:
+                temp_seq_number = temp_ack_list[temp_ack_list.index(max(temp_ack_list))-1]
+            seq_number = max(temp_ack_list)
+            print("-------2-------", temp_seq_number)
     except socket.timeout:
         if max(temp_ack_list) == seq_number:
             cache = cache[cwnd_size:]
         else:
             cache = cache[temp_ack_list.index(max(temp_ack_list)):]
+            if len(temp_ack_list) ==1:
+                temp_seq_number = max(temp_ack_list)-13
+            else:
+                temp_seq_number = temp_ack_list[temp_ack_list.index(max(temp_ack_list))-1]
+            seq_number = max(temp_ack_list)
         cwnd_size = 1
         congestion_state = 0
-        seq_number = max(temp_ack_list)
+
     batch_size = 0
     for i in range(cwnd_size):
         if i < len(cache):
             packet = cache[i]
-            seq_number = packet.sequence_number + len(packet.data)
+            if temp_seq_number == max(temp_ack_list):
+                seq_number += len(packet.data)
+            else:
+                temp_seq_number += len(packet.data)
+            print("----------3--------", temp_seq_number, seq_number)
+            if len(cache) == 1:
+                print("===================================")
+                cache = []
         else:
             packet = TcpPacket(sequence_number=seq_number, data='random packet')
             cache.append(packet)
             seq_number = seq_number + len(packet.data)
+
         sock.sendto(packet.packet, server)
