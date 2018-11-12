c852baa4b805bc6553510ded301e98cb86783690
Frosty <Jelsaoverlandfrost@gmail.com>
2018-11-13 00:23:21 +0800

    Check point

--- client.py
+++ client.py
@@ -4,75 +4,91 @@ import random
 import struct
 
 # Create a TCP/IP socket
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(0.6)
 
 
 # Connect the socket to the port where the server is listening
 
-def connect(server_address, server_port, socket):
+def connect(server_address, server_port, sock):
     server = (server_address, server_port)
-    sequence_number = random.randint(0, 10000)
-    packet = TcpPacket(syn=1, ack=0, sequence_number=sequence_number)
-    socket.sendto(packet.packet, server)
+    seq_number = random.randint(0, 10000)
+    packet = TcpPacket(syn=1, ack=0, sequence_number=seq_number)
+    sock.sendto(packet.packet, server)
     # SYN + ACK Packet
-    data, address = socket.recvfrom(4096)
+    data, address = sock.recvfrom(4096)
     header_content = struct.unpack('!LLHB', data[:11])
     recv_seq_number = header_content[0]
     recv_ack_number = header_content[1]
     recv_flag = header_content[3]
     recv_ack = recv_flag & 128
     recv_syn = recv_flag & 64
-    print(recv_ack, recv_syn)
     if recv_syn > 0 and recv_ack > 0:
         seq_num = recv_ack_number
         ack_num = recv_seq_number + 1
         expected_sequence_number = ack_num
-        last_acknowledgement_number = recv_ack_number
+        next_acknowledgement_number = recv_ack_number + 1
         packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, data='Sending data')
-        socket.sendto(packet.packet, address)
-    return expected_sequence_number, last_acknowledgement_number
+        sock.sendto(packet.packet, address)
+        return expected_sequence_number, next_acknowledgement_number, packet
 
 
-recv = connect('127.0.0.1', 20468, socket)
+recv = connect('127.0.0.1', 20468, sock)
+server = ('127.0.0.1', 20468)
 while recv is None:
-    recv = connect(socket)
-expected_sequence_number, last_acknowledgement_number = recv
-print(recv)
+    print(recv)
+    recv = connect('127.0.0.1', 20468, sock)
+expected_sequence_number, next_acknowledgement_number, packet = recv
 duplicate_counter = 0
-cache = []
+cwnd_size = 1
+congestion_state = 0
+cache = [packet]
+temp_ack_list = []
+seq_number = next_acknowledgement_number
+packet_size = 24
 
 while True:
-    data, address = socket.recvfrom(4096)
-    packet_length = len(data)
-    header_content = struct.unpack('!LLHB', data[:11])
-    recv_seq_number = header_content[0]
-    recv_ack_number = header_content[1]
-    recv_buffer = header_content[2]
-    recv_flag = header_content[3]
-    recv_ack = recv_flag & 128
-    recv_syn = recv_flag & 64
-    recv_fin = recv_flag & 32
-    print(recv_seq_number, recv_ack_number, data[11:])
-    if recv_ack > 0:
-        if last_acknowledgement_number == recv_ack_number:
-            duplicate_couter += 1
+    print(cwnd_size)
+    temp_ack_list = []
+    try:
+        for i in range(cwnd_size):
+            data, address = sock.recvfrom(4096)
+            packet_length = len(data)
+            header_content = struct.unpack('!LLHB', data[:11])
+            recv_ack_number = header_content[1]
+            temp_ack_list.append(recv_ack_number)
+            recv_buffer = header_content[2]
+        max_cwnd_size = recv_buffer // packet_size
+        print(data[11:])
+        print (seq_number)
+        print(temp_ack_list)
+        if max(temp_ack_list) == seq_number:
+            cache = cache[cwnd_size:]
+            if congestion_state:
+                cwnd_size += 1
+            else:
+                cwnd_size *= 2
+            if cwnd_size > max_cwnd_size:
+                cwnd_size = max_cwnd_size
         else:
-            duplicate_couter = 0
-            if len(cache) != 0:
-                cache.pop(0)
-        if duplicate_couter == 3:
-            seq_num = last_acknowledgement_number
-            packet = cache[0]
-            socket.sendto(packet.packet, address)
-            continue
-        if expected_sequence_number == recv_seq_number:
-            seq_num = recv_ack_number
-            ack_num = recv_seq_number + packet_length
-            expected_sequence_number = ack_num
-            packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, data="received...")
+            cache = cache[temp_ack_list.index(max(temp_ack_list)):]
+            cwnd_size = cwnd_size // 2
+            congestion_state = 1
+        seq_number = max(temp_ack_list)
+    except socket.timeout:
+        if max(temp_ack_list) == seq_number:
+            cache = cache[cwnd_size:]
+        else:
+            cache = cache[temp_ack_list.index(max(temp_ack_list)):]
+        cwnd_size = 1
+        congestion_state = 0
+    batch_size = 0
+    for i in range(cwnd_size):
+        if i < len(cache):
+            packet = cache[i]
+            seq_number = packet.sequence_number + len(packet.data)
+        else:
+            packet = TcpPacket(sequence_number=seq_number, data='random packet')
             cache.append(packet)
-        else:
-            seq_num = recv_ack_number
-            ack_num = expected_sequence_number
-            packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, data="missed...")
-        socket.sendto(packet.packet, address)
+            seq_number = seq_number + len(packet.data)
+        sock.sendto(packet.packet, server)
--- server.py
+++ server.py
@@ -1,19 +1,16 @@
 import socket
 import struct
 import random
-import time
-import queue
 from tcppacket import TcpPacket
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('127.0.0.1', 20468)
-socket.bind(server_address)
+sock.bind(server_address)
 expected_sequence_number = 0
-socket.settimeout(0.6)
 
 
-def connect(socket):
-    data, address = socket.recvfrom(4096)
+def connect(sock):
+    data, address = sock.recvfrom(4096)
     header_content = struct.unpack('!LLHB', data[:11])
     recv_seq_number = header_content[0]
     recv_ack_number = header_content[1]
@@ -25,14 +22,27 @@ def connect(socket):
         seq_num = random.randint(0, 10000)
         ack_num = recv_seq_number + 1
         packet = TcpPacket(syn=1, ack=1, sequence_number=seq_num, acknowledgement_number=ack_num)
+        sock.sendto(packet.packet, address)
+    data, address = sock.recvfrom(4096)
+    header_content = struct.unpack('!LLHB', data[:11])
+    recv_seq_number = header_content[0]
+    recv_ack_number = header_content[1]
+    recv_flag = header_content[3]
+    recv_ack = recv_flag & 128
+    recv_syn = recv_flag & 64
+    # ACK Packet
+    if recv_syn == 0 and recv_ack > 0:
+        seq_num = random.randint(0, 10000)
+        ack_num = recv_seq_number + 1
+        packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num)
         expected_sequence_number = ack_num
         last_acknowledgement_number = recv_ack_number
-        socket.sendto(packet.packet, address)
+        sock.sendto(packet.packet, address)
         return expected_sequence_number, last_acknowledgement_number
 
-recv = connect(socket)
+recv = connect(sock)
 while recv is None:
-    recv = connect(socket)
+    recv = connect(sock)
 expected_sequence_number, last_acknowledgement_number = recv
 # duplicate_couter = 0
 # cwnd_size = 1
@@ -41,8 +51,8 @@ expected_sequence_number, last_acknowledgement_number = recv
 print(recv)
 
 while True:
-    data, address = socket.recvfrom(4096)
-    packet_length = len(data)
+    data, address = sock.recvfrom(4096)
+    packet_length = len(data[11:])
     header_content = struct.unpack('!LLHB', data[:11])
     recv_seq_number = header_content[0]
     recv_ack_number = header_content[1]
@@ -57,9 +67,9 @@ while True:
             seq_num = recv_ack_number
             ack_num = recv_seq_number + packet_length
             expected_sequence_number = ack_num
-            packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, data="received...")
+            packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, buffer_size=4096)
         else:
             seq_num = recv_ack_number
             ack_num = expected_sequence_number
-            packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, data="missed...")
-    socket.sendto(packet.packet, address)
+            packet = TcpPacket(ack=1, sequence_number=seq_num, acknowledgement_number=ack_num, buffer_size=4096)
+        sock.sendto(packet.packet, address)
